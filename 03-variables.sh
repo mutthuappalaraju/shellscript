ccc4cb04dfa255fdd9b5cbb28585923c6b7b562d
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-23 17:57:44 +0530

    new coded added to shell script

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON=$2
+
+
+echo "$PERSON: hello $PERSON2,good morning"
+echo "$PERSON2: HI $PERSON1,very good morning"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: i am good $PERSON1,how are you"
+echo "$PERSON1:  good $PERSON2.how is your health"
+echo "$PERSON2: everything is fine $PERSON1"
