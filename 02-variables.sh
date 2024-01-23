746aee50ceaa19792524874aa81dc6b311dd5070
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-23 17:34:56 +0530

    new coded added to shell script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "RAMESH: hello SURESH,good morning"
+echo "SURESH: HI RAMESH,very good morning"
+echo "RAMESH: how are you SURESH"
+echo "SURESH: i am good RAMESH,how are you"
+echo "RAMESH:  good suresh.how is your health"
+echo "SURESH: everything is fine RAMESH"
