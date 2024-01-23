866b4ceab72cb19c1f69037771c438d3518f6917
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-23 17:48:53 +0530

    new coded added to shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,9 +3,9 @@
 PERSON1=RAMESH
 PERSON2=SURESH
 
-echo "$1: hello $2,good morning"
-echo "$2: HI $1,very good morning"
-echo "$1: how are you $2"
-echo "$2: i am good $1,how are you"
-echo "$1:  good $2.how is your health"
-echo "$2: everything is fine $1"
+echo "$PERSON: hello $PERSON2,good morning"
+echo "$PERSON2: HI $PERSON1,very good morning"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: i am good $PERSON1,how are you"
+echo "$PERSON1:  good $PERSON2.how is your health"
+echo "$PERSON2: everything is fine $PERSON1"
