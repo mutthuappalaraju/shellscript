f2933aaada5550d29feb60e353bc9e343a952bfc
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-24 16:55:48 +0530

    new coded added to shell script

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -12,3 +12,14 @@ else
 fi
 
 yum install mysql -y
+
+if [$ID -ne 0]
+then
+    echo "ERROR: : run this script root user"
+    exit 1
+
+else
+    echo "you are the root user"
+fi
+
+yum install git -y
