921cade9848441f4f2900e3e0282de91a7d82eb9
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-24 17:34:38 +0530

    new coded added to shell script

---
+++ 09-function.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ID=$(id -u)
+
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR: : installing  failure"
+        exit 1
+
+    else
+        echo "installing  success"
+    fi
+}
+if [ $ID -ne 0]
+then 
+    echo "ERROR: : run the script root user"
+    exit 1
+else 
+    echo "root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
