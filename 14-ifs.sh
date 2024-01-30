63343e30b9d59cfa285962ac99a15ea2487fa6ce
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-30 16:24:38 +0530

    frist roboshop shell

---
+++ 14-ifs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FILE=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if ( -f $file )
+then
+    echo -e "$R failure:$file doesnot exist..$N"
+fi
+
+while IFS=":" read -r Username Password User ID  Group ID User ID Info
+do
+    echo "username: $username"
+    echo "password: $password"
+    echo "user id: $user id"
+
+done < $file
