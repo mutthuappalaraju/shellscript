a2df7c8b5b633eb7a4bd5aa282ce132bd18412fb
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-31 17:11:15 +0530

    frist roboshop shell

--- 16-diskusagecls.sh
+++ 16-diskusagecls.sh
@@ -9,8 +9,8 @@ do
     usage=$( df -hT | grep -vE  'tmp|file' | awk '{print $3f}' | cut -d % -f1 )
     partition=$(echo $line  | awk '{print $1f}')
     
-    if [ "$usage" -ge "$DISK_THERSOULD" ]
+    if [ $usage -ge $DISK_THERSOULD ]
     then 
-        message= "high disk usage on $partition:$usage"
+        message+="high disk usage on $partition:$usage"
     fi
 done <<< $DISK_USAGE
