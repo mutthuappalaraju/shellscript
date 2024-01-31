85dcac16a8bfb7df06dabc7f1eb1d4d389e3d88a
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-31 16:56:32 +0530

    frist roboshop shell

---
+++ 16-diskusagecls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE  'tmp|file')
+DISK_THERSOULD=1
+MESSAGE=""
+
+while ifs= read line
+
+do 
+    usage=$( echo $line | awk '{print $3f}' | cut -d % -f1 )
+    if [ $usage -ge $DISK_THERSOULD ]
+    then 
+        echo "high disk usage"
+    fi
+done
