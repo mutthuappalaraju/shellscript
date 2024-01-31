54777662faeee3e8459c0a01d5d2394c1c964aed
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-31 17:14:58 +0530

    frist roboshop shell

--- 16-diskusagecls.sh
+++ 16-diskusagecls.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -vE  'tmp|file')
-DISK_THERSOULD=1
-MESSAGE=""
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
 
-while ifs= read line
-do 
-    usage=$( df -hT | grep -vE  'tmp|file' | awk '{print $3f}' | cut -d % -f1 )
-    partition=$(echo $line  | awk '{print $1f}')
-    
-    if [ "$usage" -ge "$DISK_THERSOULD" ]
-    then 
-        message+="high disk usage on $partition:$usage \n"
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
 done <<< $DISK_USAGE
