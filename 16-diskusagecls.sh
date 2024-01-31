cbfe8303dcbbf3879977bfc0485a112bbcc656cd
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-31 17:41:09 +0530

    frist roboshop shell

--- 16-diskusagecls.sh
+++ 16-diskusagecls.sh
@@ -4,12 +4,14 @@ DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=""
 
-while IFS= read line
+while ifs= read line 
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1F}')
+    usage=$(echo $line |  awk '{print $6f}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1f}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage"
+        message="high disk usage on $partition:$usage\n"
     fi
-done <<< $DISK_USAGE
+done << $DISK_USAGE
+
+echo -e "message:$message"
