3804208a7bef8c1857253acc582672d06479b7be
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-31 16:42:37 +0530

    frist roboshop shell

--- 15-diskusage.sh
+++ 15-diskusage.sh
@@ -4,7 +4,7 @@ thershould=1
 
 disk_usage=$(df -hT | grep -vE  'tmp|file' | awk '{print $6f}' | cut -d % -f1)
 
-if [ $disk_usage -ge $thershould ]
+if [ "$disk_usage" -ge "$thershould" ]
 then
     echo "disk_usage above $thershould%"
 else
